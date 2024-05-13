86980d29ce85a087a5c46b11a9ee7ab9de9b0b3f
SAJID <sajid.munir4428@hotmail.com>
2024-05-13 18:55:04 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import './App.css'
 import './global.css';
 import MainBanner from './components/MainBanner';
-import SkillSection from './components/SkillSection';
 import ProjectSection from './components/ProjectSection';
 import Header from './components/Header';
 import Footer from './components/Footer';
