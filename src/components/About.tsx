b2d0a7b790e3ec9b1fbd99325c191fa2bb3a54ee
SAJID <sajid.munir4428@hotmail.com>
2024-06-03 16:44:32 +0500

    updates

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,8 +3,8 @@ import styles from "./About.module.css";
 function About() {
     return (<div className={styles.container} id="about">
         <h2 className={styles.heading}>About Me</h2>
-        <p className={styles.text}>I am a curios, good listener and happen to have a problem solving mindset.
-         I believe tha one should be able to assess the value of the output of each choice or decision in profressional or personal life 
+        <p className={styles.text}>I am curios, good listener and happen to have a problem solving mindset.
+         I believe that one should be able to assess the value of the output of each choice or decision in profressional or personal life 
             and come with a best value approach which is feasible as well.
         </p>
     </div>);
--- src/components/MainBanner.tsx
+++ src/components/MainBanner.tsx
@@ -7,6 +7,7 @@ function MainBanner() {
         <p className={styles.text}>I enjoy working as a web developer,
             and with the <span className={styles.highlightColorOne}>motivation and eagerness</span> to work on different stacks and gain practical knowledge, I believe I can
             be a <span className={styles.highlightColorOne}>valuable asset</span> to my <span className={styles.highlightColorTwo}>potential team.</span>
+            <p>The best thing I love about Web development in particular is how evolving this field is.</p>
         </p>
     </motion.div>);
 }
