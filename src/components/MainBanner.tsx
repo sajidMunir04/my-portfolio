8cffab7e57e1d7ad7f05a03baf574d5bb840b706
SAJID <sajid.munir4428@hotmail.com>
2024-06-20 17:36:54 +0500

    design fixes

--- src/components/MainBanner.tsx
+++ src/components/MainBanner.tsx
@@ -6,7 +6,6 @@ function MainBanner() {
         <h1 className={styles.mainText}>Sajid Munir</h1>
         <p className={styles.text}>What I love most is solving problems and love the satisfaction which
             comes after systematically approaching a solution.
-            <p>The best thing I love about Web development in particular is how evolving this field is.</p>
         </p>
     </motion.div>);
 }
