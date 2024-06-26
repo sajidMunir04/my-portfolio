84321cef08c5f2127fcf2798c8d47ce4ae7b8dbb
SAJID <sajid.munir4428@hotmail.com>
2024-06-26 15:11:27 +0500

    changed images to webp for faster loading

--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -13,22 +13,22 @@ function ProjectSection() {
         <div className={styles.contentContainer}>
             <ProjectCard projectHeading={"Luminae - A brand's ecommerce store"}
             projectDescription={"This is a full stack e-commerce website developed on Next.js utilzing serverless backend with MongoDB as database"}
-            imageLink={'/images/luminae.png'} techStacks={luminaeTechStacks} sourceLink={'https://github.com/sajidMunir04/luminae'} demoLink={"https://luminaed.vercel.app/"}/>
+            imageLink={'/images/luminae.webp'} techStacks={luminaeTechStacks} sourceLink={'https://github.com/sajidMunir04/luminae'} demoLink={"https://luminaed.vercel.app/"}/>
             <ProjectCard projectHeading={"Questeria (In development)"}
             projectDescription={"It is an application in which user can create forms by selecting different types of question, using express.js as backend."}
             imageLink={'/images/questeria.webp'} techStacks={questeriaTechStacks} sourceLink={'https://github.com/sajidMunir04/questeria'} demoLink={''}/>
             <ProjectCard projectHeading={"Little Learners - A school"}
             projectDescription={"When I saw this figma design, its uniqueness compelled me do this complete frontend design in react which is fully responsive"}
-            imageLink={'/images/littlelearners.png'} techStacks={littleLearnersTechStacks} sourceLink={'https://github.com/sajidMunir04/little-learners'} demoLink={'https://littlelearner.netlify.app'}/>
+            imageLink={'/images/littlelearners.webp'} techStacks={littleLearnersTechStacks} sourceLink={'https://github.com/sajidMunir04/little-learners'} demoLink={'https://littlelearner.netlify.app'}/>
             <ProjectCard projectHeading={"Your Bank - An online bank"}
             projectDescription={"After using wise.com, I was searching for a similar design to do in frontend, and this was the closest which I found, and it gave me a ground to practice alot of my skills"}
-            imageLink={'/images/yourbank.png'} techStacks={yourBankTechStacks} sourceLink={'https://github.com/sajidMunir04/your-bank'} demoLink={'https://demosite123456.netlify.app/'}/>
+            imageLink={'/images/yourbank.webp'} techStacks={yourBankTechStacks} sourceLink={'https://github.com/sajidMunir04/your-bank'} demoLink={'https://demosite123456.netlify.app/'}/>
             <ProjectCard projectHeading={"SkillBridge - An online courses institute"}
             projectDescription={"This is my first React based project and I used chose Javascript for starting out with React. It gave me a lot of exposure to basic React principles and hooks mainly"}
-            imageLink={'/images/skillbridge.png'} techStacks={skillbridgeTechStacks} sourceLink={'https://github.com/sajidMunir04/skillbridge'} demoLink={'https://skilledbridge.netlify.app'}/>
+            imageLink={'/images/skillbridge.webp'} techStacks={skillbridgeTechStacks} sourceLink={'https://github.com/sajidMunir04/skillbridge'} demoLink={'https://skilledbridge.netlify.app'}/>
             <ProjectCard projectHeading={"Streamvibe (In development)"}
             projectDescription={"This is my first angular project, which gave me alot of exposure to Angular framework, this is still in development."}
-            imageLink={'/images/streamvibe.png'} techStacks={streamvibeTechStacks} sourceLink={'https://github.com/sajidMunir04/stream-vibe'} demoLink={'https://streamvibes.netlify.app'}/>
+            imageLink={'/images/streamvibe.webp'} techStacks={streamvibeTechStacks} sourceLink={'https://github.com/sajidMunir04/stream-vibe'} demoLink={'https://streamvibes.netlify.app'}/>
         </div>
     </div>);
 }
