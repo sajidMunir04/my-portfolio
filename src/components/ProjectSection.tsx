6e32da4771ffba5b341c7ee46edf779d466d4363
SAJID <sajid.munir4428@hotmail.com>
2024-07-07 16:01:10 +0500

    Update ProjectSection.tsx

--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -14,12 +14,12 @@ function ProjectSection() {
             <ProjectCard projectHeading={"Luminae - A brand's ecommerce store"}
             projectDescription={"This is a full stack e-commerce website developed on Next.js utilzing serverless backend with MongoDB as database"}
             imageLink={'/images/luminae.webp'} techStacks={luminaeTechStacks} sourceLink={'https://github.com/sajidMunir04/luminae'} demoLink={"https://luminaed.vercel.app/"}/>
-            <ProjectCard projectHeading={"Questeria (In development)"}
-            projectDescription={"It is an application in which user can create forms by selecting different types of question, using express.js as backend."}
-            imageLink={'/images/questeria.webp'} techStacks={questeriaTechStacks} sourceLink={'https://github.com/sajidMunir04/questeria'} demoLink={''}/>
             <ProjectCard projectHeading={"Little Learners - A school"}
             projectDescription={"When I saw this figma design, its uniqueness compelled me do this complete frontend design in react which is fully responsive"}
             imageLink={'/images/littlelearners.webp'} techStacks={littleLearnersTechStacks} sourceLink={'https://github.com/sajidMunir04/little-learners'} demoLink={'https://littlelearner.netlify.app'}/>
+            <ProjectCard projectHeading={"Questeria (In development)"}
+            projectDescription={"It is an application in which user can create forms by selecting different types of question, using express.js as backend."}
+            imageLink={'/images/questeria.webp'} techStacks={questeriaTechStacks} sourceLink={'https://github.com/sajidMunir04/questeria'} demoLink={''}/>
             <ProjectCard projectHeading={"Your Bank - An online bank"}
             projectDescription={"After using wise.com, I was searching for a similar design to do in frontend, and this was the closest which I found, and it gave me a ground to practice alot of my skills"}
             imageLink={'/images/yourbank.webp'} techStacks={yourBankTechStacks} sourceLink={'https://github.com/sajidMunir04/your-bank'} demoLink={'https://demosite123456.netlify.app/'}/>
