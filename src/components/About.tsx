dc258df4c7261a5f900e98b88562f8c467a18210
SAJID <sajid.munir4428@hotmail.com>
2024-07-24 07:50:23 +0500

    updates

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,7 +3,7 @@ import styles from "./About.module.css";
 function About() {
     return (<div className={styles.container} id="about">
         <h2 className={styles.heading}>About Me</h2>
-        <p className={styles.text}>I discovered my passion for web development, when I realized how these solutions are helping people every day, and now when I have completed my courses and with several projects under my belt and most of my work on end-to-end development, I am looking for an entry level opportunity to be a part of a team, where I can grow and make an impact.
+        <p className={styles.text}>I discovered my passion for web development when I realized how these products are helping people every day. Now that I have completed my courses and with several projects under my belt and most of my work in full stack development, I am looking for an entry-level opportunity to be a part of a team, where I can grow and provide my maximum value.
         </p>
     </div>);
 }
--- src/components/MainBanner.tsx
+++ src/components/MainBanner.tsx
@@ -4,8 +4,7 @@ import { motion } from 'framer-motion';
 function MainBanner() {
     return (<motion.div className={styles.container}>
         <h1 className={styles.mainText}>Sajid Munir</h1>
-        <p className={styles.text}>What I love most is solving problems and love the satisfaction which
-            comes after systematically approaching a solution.
+        <p className={styles.text}>A passionate developer who wants to learn and grow
         </p>
     </motion.div>);
 }
