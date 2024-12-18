200451628425e986e42d7c9fb24ed8afcddbb307
Sajid Munir <sajid.munir4428@hotmail.com>
2024-12-18 20:15:20 +0500

    Update MainBanner.tsx

--- src/components/MainBanner.tsx
+++ src/components/MainBanner.tsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion';
 function MainBanner() {
     return (<motion.div className={styles.container}>
         <h1 className={styles.mainText}>Sajid Munir</h1>
-        <p className={styles.text}>I love solving problem and creating amazing user interfaces
+        <p className={styles.text}>I love solving problems and creating amazing user interfaces
         </p>
     </motion.div>);
 }
